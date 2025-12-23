d7884795f4ec2c7917ad086f5151fb701d59f068
nethaka-sades <sades.dev01@gmail.com>
2025-12-24 02:29:59 +0530

    added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,14 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Navbar_Main from "@/components/main/NavBar";
-import { ThemeProvider } from "@/components/ui/theme-provider";
 import { Hubot_Sans } from "next/font/google";
-import Lenis from "lenis";
-import SmoothScrolling from "@/components/common/smooth-scrolling";
-import { BoxReveal } from "@/components/common/box-reveal";
-import { Footer } from "@/components/main/Footer";
-import { StackedCircularFooter } from "@/components/common/footer_center";
 
 const cabinSans = Hubot_Sans({
   subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "MCMU",
-  description: "Media Unit",
+  title: "Mahanama College Media Unit",
+  description: "Official Web Portal of Mahanama CollegeMedia Unit",
 };
 
 export default function RootLayout({
@@ -25,6 +18,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <link
+          rel="icon"
+          href="/logos/MCMU_GOLD_LOGO.png"
+          type="image/x-icon"
+        />
+      </head>
       <body className={`${cabinSans.className} antialiased`}>
         {children}
       </body>
